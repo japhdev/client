5fdbce31879e3e46b9575276cedee02f9a77e5bd
Alan <133844140+Alanph18@users.noreply.github.com>
2025-12-07 15:55:07 -0600

    Refactor Profile component: standardize quotes and formatting for consistency

--- src/PorfolioContainer/Home/Profile.js
+++ src/PorfolioContainer/Home/Profile.js
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
     return (
-        <div className='profile-container'>
-            <div className='profile-parent'>
-                <div className='profile-details'>
-                    <div className='colz'>
-                        <a href='https://github.com/japhdev'>
-                            <i className='fa fa-github'></i>
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href="https://github.com/japhdev">
+                            <i className="fa fa-github"></i>
                         </a>
-                        <a href='#'>
-                            <i className='fa fa-google'></i>
+                        <a href="#">
+                            <i className="fa fa-google"></i>
                         </a>
-                        <a href='#'>
-                            <i className='fa fa-instagram'></i>
+                        <a href="#">
+                            <i className="fa fa-instagram"></i>
                         </a>
-                        <a href='#'>
-                            <i className='fa fa-twitter'></i>
+                        <a href="#">
+                            <i className="fa fa-twitter"></i>
                         </a>
+                    </div>
+
+                    <div className="profile-details-name">
+                        <span className="primary-text">
+                            {""}
+                            Hello, I'M <span className="highlighted-text">JAPH</span>
+                        </span>
                     </div>
                 </div>
             </div>
-        
         </div>
-    )
+    );
 }
