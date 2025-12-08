83ba60357f28fcc5f51264b6624a8b09a7c19c00
Alan <133844140+Alanph18@users.noreply.github.com>
2025-12-08 16:21:56 -0600

    Fix typographical errors in import paths for Home in App.js and commonUtils.js, correcting “Porfolio” to “Portfolio”

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Home from './PorfolioContainer/Home/Home';
+import Home from './PortfolioContainer/Home/Home';
 
 function App() {
   return (
--- src/utilities/commonUtils.js
+++ src/utilities/commonUtils.js
@@ -1,4 +1,4 @@
-import Home from "../PorfolioContainer/Home/Home";
+import Home from "../PortfolioContainer/Home/Home";
 
 
 export const TOTAL_SCREENS = [
