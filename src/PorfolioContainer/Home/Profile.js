c1379fdf48606ee99841ac5ca8a843fde65ff464
Alan <133844140+Alanph18@users.noreply.github.com>
2025-12-07 20:01:58 -0600

    Refactor Profile component: improve formatting and structure for social icons and add profile picture section

--- src/PorfolioContainer/Home/Profile.js
+++ src/PorfolioContainer/Home/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {ReactTyped} from "react-typed"
+import { ReactTyped } from "react-typed";
 
 export default function Profile() {
     return (
@@ -7,18 +7,20 @@ export default function Profile() {
             <div className="profile-parent">
                 <div className="profile-details">
                     <div className="colz">
-                        <a href="https://github.com/japhdev">
-                            <i className="fa fa-github"></i>
-                        </a>
-                        <a href="#">
-                            <i className="fa fa-google"></i>
-                        </a>
-                        <a href="#">
-                            <i className="fa fa-instagram"></i>
-                        </a>
-                        <a href="#">
-                            <i className="fa fa-twitter"></i>
-                        </a>
+                        <div className="colz-icon">
+                            <a href="https://github.com/japhdev">
+                                <i className="fa fa-github"></i>
+                            </a>
+                            <a href="#">
+                                <i className="fa fa-google"></i>
+                            </a>
+                            <a href="#">
+                                <i className="fa fa-instagram"></i>
+                            </a>
+                            <a href="#">
+                                <i className="fa fa-twitter"></i>
+                            </a>
+                        </div>
                     </div>
 
                     <div className="profile-details-name">
@@ -33,18 +35,17 @@ export default function Profile() {
                             <h1>
                                 {""}
                                 <ReactTyped
-                                strings={[
-                                    "Software Engineer",
-                                    "Backend Developer",
-                                    "Web Developer",
-                                    "Mobile Developer",
-                                ]}
-                                typeSpeed={50}
-                                backSpeed={50}
-                                backDelay={3000} 
-                                loop
+                                    strings={[
+                                        "Software Engineer",
+                                        "Backend Developer",
+                                        "Web Developer",
+                                        "Mobile Developer",
+                                    ]}
+                                    typeSpeed={50}
+                                    backSpeed={50}
+                                    backDelay={3000}
+                                    loop
                                 />
-                            
                             </h1>
                             <span className="profile-role-tagline">
                                 Building the future, one idea at a time.
@@ -54,12 +55,18 @@ export default function Profile() {
                     <div className="profile-options">
                         <button className="btn primary-btn">
                             {""}
-                            Contact Me {" "}
+                            Contact Me{" "}
                         </button>
-                        <a href="CurriculumVitae.pdf" download="Josue Alan Pablo Hernandez.pdf">
+                        <a
+                            href="CurriculumVitae.pdf"
+                            download="Josue Alan Pablo Hernandez.pdf"
+                        >
                             <button className="btn highlighted-btn">Get Resume</button>
                         </a>
                     </div>
+                </div>
+                <div className="profile-picture">
+                    <div className="profile-picture-background"></div>
                 </div>
             </div>
         </div>
