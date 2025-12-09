311ae996f1a1fa69b326129ce948de9114583cc1
Alan <133844140+Alanph18@users.noreply.github.com>
2025-12-08 20:28:01 -0600

    Add const switchScreen and div for navbar with logo

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { TOTAL_SCREENS, GET_SCREEN_INDEX } from '../../../utilities/commonUtils'
 import ScrollService from '../../../utilities/ScrollService'
-import { faBars } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Header.css'
 
@@ -16,32 +16,57 @@ export default function Header() {
         if (screenIndex < 0)
             return
     }
+
     let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
 
-    const getHeaderOptions = ()=>{
-        return(
-            TOTAL_SCREENS.map((screen, i)=>(
+    const getHeaderOptions = () => {
+        return (
+            TOTAL_SCREENS.map((screen, i) => (
                 <div key={screen.screen_name} className={getHeaderOptionsClass(i)}
-                onClick={() => switchScreen(i, screen)}>
+                    onClick={() => switchScreen(i, screen)}>
                     <span>{screen.screen_name}</span>
                 </div>
             ))
         )
     }
 
-    const getHeaderOptionsClass =(index)=>{
+    const getHeaderOptionsClass = (index) => {
         let classes = "header-option";
-        if(index < TOTAL_SCREENS.length -1)
+        if (index < TOTAL_SCREENS.length - 1)
             classes += "header-option-separator";
 
-        if(selectedScreen === index)
+        if (selectedScreen === index)
             classes += "selected-header-option";
         return
+    }
+
+    const switchScreen = (index, screen) => {
+        let screenComponent = document.getElementById(screen.screen_name);
+        if (!screenComponent) return;
+
+        screenComponent.scrollIntoView({ behavior: 'smooth' });
+        setSelectedScreen(index);
+        setShowHeaderOptions(false);
     }
 
 
     return (
         <div>
+            <div className='header-container' onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
+                <div className='header-parent'>
+                    <div className='header-hamburger' onClick={() => setShowHeaderOptions(!setShowHeaderOptions)}>
+                        <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+                    </div>
+                    <div className='header-logo'>
+                        <span>JAPH Dev</span>
+                    </div>
+                    <div className={(showHeaderOptions) ? "header-options show-hamburger-options" : Header}>
+                        {getHeaderOptions()}
+                    </div>
+                </div>
+
+
+            </div>
 
         </div>
     )
